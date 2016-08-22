de12a65dcd0c968790ff4bef88305a54c70597de
zzhy1996 <zzhy_1996@hotmail.com>
2016-08-22 12:18:09 +0800

    add some modes

--- qzonelib.py
+++ qzonelib.py
@@ -1,3 +1,7 @@
+"""
+MIT LICENSE
+"""
+
 import json
 
 from qqlib import qzone
@@ -17,26 +21,27 @@ class QQManager(object):
     def loadQQFromConf(self):
         conf_ = open(self.conf)
         for line in conf_.readlines():
-            [qq, pwd] = line.split(":")
-            qq = str(qq).lstrip().rstrip()
+            [qq_, pwd] = line.split(":")
+            qq_ = str(qq_).lstrip().rstrip()
             pwd = str(pwd).lstrip().rstrip()
-            self.qqSet.add((qq, pwd))
+            self.qqSet.add((qq_, pwd))
 
     def hasQQ(self):
         return len(self.qqSet) != 0
 
     def nextQQ(self):
-        qq = None
+        qq_ = None
         if self.hasQQ():
-            qq = self.qqSet.pop()
-            self.usedQQSet.add(qq)
-        print('已取得下一个qq信息： qq：%s, pwd:%s' % (qq[0], qq[1]))
-        return qq
+            qq_ = self.qqSet.pop()
+            self.usedQQSet.add(qq_)
+        print('已取得下一个qq信息： qq：%s, pwd:%s' % (qq_[0], qq_[1]))
+        return qq_
 
 
 ALL = 0  # 爬取提供qq的所有说说，默认
-KEYWORD = 1  # 根据提供的关键字来
-KEYWORD_ALL = 2  # 暂未设计该模式
+KEYWORD = 1  # 根据提供的关键字来点赞，仅点赞最近一条
+KEYWORD_ALL = 2  # 为包含该关键字的所有说说点赞
+LATEST = 3  # 为最近的一条说说点赞
 
 
 class QzoneLiker(object):
@@ -45,7 +50,7 @@ class QzoneLiker(object):
         self.qqClient = None
         self.keyword = None
         self.qqLogin = None
-        self.tid = None
+        self.limit = -1
         self.mode = mode
 
     def __likeALL(self, tids):
@@ -53,6 +58,11 @@ class QzoneLiker(object):
             self.__like(tid)
 
     def __like(self, tid):
+        """
+        点赞说说
+        :param tid:
+        :return:
+        """
         url = 'http://h5.qzone.qq.com/proxy/domain/w.qzone.qq.com/cgi-bin/likes/internal_dolike_app'
         resp = self.qqClient.fetch(url, params={
             'g_tk': self.qqClient.g_tk()
@@ -71,9 +81,8 @@ class QzoneLiker(object):
 
     def __loadShuoJson(self, pos, num):
         """
-
+        加载json
         :param self:
-        :param qq:
         :param pos:
         :param num:
         :return:
@@ -97,6 +106,14 @@ class QzoneLiker(object):
         return self.qqClient.fetch(url=shuoshuourl, data=None, params=data)
 
     def __getMsgList(self, pos, num):
+        """
+        通过调用 r = self.__loadShuoJson(pos=pos, num=num) 来获取伪json，
+        如果响应码为200 则解析json，返回 msglist
+
+        :param pos:
+        :param num:
+        :return: msglist
+        """
         r = self.__loadShuoJson(pos=pos, num=num)
 
         if r.status_code == 200:
@@ -111,15 +128,38 @@ class QzoneLiker(object):
                 return None
             return msglist
 
+    def __getTids(self, pos, num):
+        msglist = self.__getMsgList(pos, num)
+        tids = list()
+        for msg in msglist:
+            tids.append(msg['tid'])
+        return tids
+
     def __getTidsAll(self):
+        """
+        通过调用 self.__getMsgList(pos, num) 来获取msglist
+        :return:
+        """
         pos = 0
         num = 20
         tids = list()
+        isKeywordAll = self.mode == KEYWORD_ALL
+        last = False
         while True:
+            if last:
+                break
+            if self.limit != -1:  # 是否设置了Limit
+                if pos < self.limit < pos + num:
+                    num = self.limit - pos
+                    last = True
+
             msglist = self.__getMsgList(pos, num)
             if msglist is None:
                 break
             for msg in msglist:
+                if isKeywordAll:
+                    if self.keyword not in msg['content']:
+                        continue
                 tids.append(msg['tid'])
             pos += num
         return tids
@@ -128,7 +168,12 @@ class QzoneLiker(object):
         pos = 0
         num = 20
 
-        while not self.tid:  # tid为None
+        found = False
+        isKeyword = self.mode == KEYWORD
+        if not isKeyword:
+            return None
+
+        while not found:
             msglist = self.__getMsgList(pos, num)
             if msglist is None:
                 break
@@ -137,15 +182,23 @@ class QzoneLiker(object):
                     self.tid = msg['tid']
                     print('已取得bc包含关键字“%s”说说id...%s' % (self.keyword, self.tid))
                     return self.tid
-                    # print(msg['content'])
-                    # print(msg['tid'])
-            pos += num
+        pos += num
 
-    def setKeyWord(self, keyword):
-        self.keyword = keyword
+    def setLimit(self, limit):
+        self.limit = limit
+        return self
+
+    def setKeyWord(self, keyword_):
+        self.keyword = keyword_
         return self
 
     def login(self, qq_, pwd):
+        """
+        使用qqlib来登陆空间，使用该缓存点赞说说
+        :param qq_:
+        :param pwd:
+        :return:
+        """
         self.qqClient = qzone.QZone(qq_, pwd)
         self.qqClient.login()
         self.qqLogin = qq_
@@ -153,19 +206,26 @@ class QzoneLiker(object):
         return self
 
     def like(self):
-        if self.mode == KEYWORD:
-            if self.keyword is not None:
-                tid = self.__getTidByKeyword()
-                if tid is not None:
-                    self.__like(tid)
-            else:
+        """
+        供用户调用api
+        为之前所设置的符合条件的说说点赞
+        :return:
+        """
+        if self.mode == KEYWORD or self.mode == KEYWORD_ALL:
+            if self.keyword is None:
                 print("请使用setKeyword()来设置关键字")
+                return None
 
-        elif self.mode == ALL:
+        if self.mode == KEYWORD:
+            tid = self.__getTidByKeyword()
+            if tid is not None:
+                self.__like(tid)
+        elif self.mode == ALL or self.mode == KEYWORD_ALL:
             tids = self.__getTidsAll()
             self.__likeALL(tids)
-        elif self.mode == KEYWORD_ALL:
-            pass
+        elif self.mode == LATEST:
+            tid = self.__getTids(0, 1)[0]
+            self.__like(tid)
         else:
             ModeNotFoundError()
 
@@ -180,13 +240,16 @@ class QzoneLiker(object):
 if __name__ == '__main__':
     conf = 'qqlist.ini'  # qq号配置文件，多qq批量点赞
 
-    qq = 2037379421  # 被点赞的qq号
+    qq = '10001'  # 被点赞的qq号
     keyword = 'keyword'  # 说说关键字，ALL模式下无需指定
 
     liker = QzoneLiker(qq, mode=ALL)  # 此模式为默认模式，无需设置keyword
+    liker = QzoneLiker(qq, mode=LATEST)  # 为最近的一条说说点赞
     liker = QzoneLiker(qq, mode=KEYWORD).setKeyWord(keyword)  # 必须设置keyword
+    liker = QzoneLiker(qq, mode=KEYWORD_ALL).setKeyWord(keyword)  # 必须设置keyword
+
     # 单独登陆指定qq
-    liker.login('10000', 'pwdpwdpwd')
+    liker.login('10000', 'password')
     liker.like()
     # 可以批量处理指定配置文件中的qq号信息
     liker.likeBatch(conf)
